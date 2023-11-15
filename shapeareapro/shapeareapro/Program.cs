59fb9c86eb0ef41d0b750ed1cee878c3465ca3af
sharma hemant <96920489+hemant2189@users.noreply.github.com>
2023-11-15 11:40:05 +0530

    Add files via upload

---
+++ shapeareapro/shapeareapro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapeareapro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            float result;
+
+            Console.WriteLine("What shape would you lije to find the area of? Please enter 'r' for rectange or any other key for the circle!.");
+
+            answer = Console.ReadLine();
+
+            if(answer == "r")
+            {
+                Console.WriteLine("please enter the height of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("please enter the width of the rectangle");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter the radius of the circle!");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+
+            Console.WriteLine("The result is: " + result);
+            Console.ReadKey();
+        }
+    }
+}
